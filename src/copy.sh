f90a1c63e3a0a756281a9d99d16c6a8f31bbef98
FortGrace <126282334+FortGrace@users.noreply.github.com>
2023-03-11 23:18:25 +0700

    Update copy.sh

--- src/copy.sh
+++ src/copy.sh
@@ -4,6 +4,7 @@
 # ssh-copy-id alexandr@192.168.110.129
 # ssh alexandr@192.168.110.129
 
+sshpass -p Privetded1
 scp src/cat/s21_cat alexandr@192.168.110.129:~/
 scp src/grep/s21_grep alexandr@192.168.110.129:~/
-ssh alexandr@192.168.110.129 "echo "Privetded1" | sudo -S mv s21_cat s21_grep /usr/local/bin"
+#ssh alexandr@192.168.110.129 "echo "Privetded1" | sudo -S mv s21_cat s21_grep /usr/local/bin"
